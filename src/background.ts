84859b1f71e525f44efd8a995f191cdcd7109234
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2024-12-27 23:35:56 +0900

    inject prompt button

--- src/background.ts
+++ src/background.ts
@@ -5,3 +5,11 @@ console.log("Hello from the background!");
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+browser.runtime.onMessage.addListener((message: any) => {
+  if (message.action === "openOptions") {
+    browser.runtime.openOptionsPage();
+  }
+  return true;
+});
